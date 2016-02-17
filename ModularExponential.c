c6f35198cc7f2aa0c6e8989705cf9b35192af837
Prajjawal Agarwal <prajjawalagarwal@gmail.com>
2016-02-17 12:30:31 +0530

    Create ModularExponential.c

---
+++ ModularExponential.c
@@ -0,0 +1,11 @@
+int power(int a,int b)
+{
+	int mod=11;
+	if(b==0)
+		return 1;
+	int x=power(a,b/2);
+	if(b%2==0)
+		return ((x%mod)*(x%mod))%mod;//if power(a,b/2)*power(a,b/2) then more time;
+	else
+		return ((a%mod)*(x%mod)*(x%mod))%mod;//log b
+}
